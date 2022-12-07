e92b51efe3cad53873ed41f5870ba325b18f4cc3
Hiroki Osame <hiroki.osame@gmail.com>
2022-12-07 14:35:47 +0900

    feat: log results

--- tests/index.ts
+++ tests/index.ts
@@ -11,8 +11,8 @@ test('Should prevent console.log hijack', async () => {
 
 	expect(testProcess.exitCode).toBe(0);
 	expect(testProcess.stdout).toMatch('✔ should log\n');
-	expect(testProcess.stdout).toMatch('1 passing');
-	expect(testProcess.stdout).not.toMatch('failing');
+	expect(testProcess.stdout).toMatch('1 passed');
+	expect(testProcess.stdout).not.toMatch('failed');
 });
 
 test('Failures should exit with 1', async () => {
@@ -23,7 +23,7 @@ test('Failures should exit with 1', async () => {
 
 	expect(testProcess.exitCode).toBe(1);
 	expect(testProcess.stderr).toMatch('Expected: 2');
-	expect(testProcess.stdout).toMatch('0 passing\n1 failing');
+	expect(testProcess.stdout).toMatch('0 passed\n1 failed');
 });
 
 test('synchronous', async () => {
@@ -31,7 +31,7 @@ test('synchronous', async () => {
 
 	expect(testProcess.exitCode).toBe(0);
 	expect(testProcess.stdout).toMatch('a\nb\nc\n✔ Async\n✔ B\n✔ C');
-	expect(testProcess.stdout).toMatch('3 passing');
+	expect(testProcess.stdout).toMatch('3 passed');
 });
 
 describe('asynchronous', ({ test }) => {
@@ -40,8 +40,8 @@ describe('asynchronous', ({ test }) => {
 
 		expect(testProcess.exitCode).toBe(0);
 		expect(testProcess.stdout).toMatch(/✔ A\n✔ Group › B\n✔ Group › B\n✔ Group - async › C\n✔ Group - async › D\n✔ Group - async › Test suite - Group › A\n✔ Group - async › Test suite - Group › B\n✔ Group - async › Test suite - Group Async › C\n✔ Group - async › Test suite - Group Async › D\n✔ Group - async › Test suite - E \(\d+ms\)\n✔ E/);
-		expect(testProcess.stdout).toMatch('11 passing');
-		expect(testProcess.stdout).not.toMatch('failing');
+		expect(testProcess.stdout).toMatch('11 passed');
+		expect(testProcess.stdout).not.toMatch('failed');
 	});
 
 	test('concurrent', async () => {
@@ -49,8 +49,8 @@ describe('asynchronous', ({ test }) => {
 
 		expect(testProcess.exitCode).toBe(0);
 		expect(testProcess.stdout).toMatch('✔ B\n✔ C\n✔ A');
-		expect(testProcess.stdout).toMatch('3 passing');
-		expect(testProcess.stdout).not.toMatch('failing');
+		expect(testProcess.stdout).toMatch('3 passed');
+		expect(testProcess.stdout).not.toMatch('failed');
 	});
 
 	test('timeout', async () => {
