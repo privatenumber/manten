49c896a71a6405f81ed56505d7f80f6c61c80084
Hiroki Osame <hiroki.osame@gmail.com>
2023-11-08 13:49:56 +0900

    style: lintfix

--- tests/specs/test-suite-2.ts
+++ tests/specs/test-suite-2.ts
@@ -1,7 +1,7 @@
 import { setTimeout } from '../utils/set-timeout.js';
 import { testSuite } from '#manten';
 
-export default testSuite(({ describe, test }) => {
+export default testSuite(({ describe }) => {
 	describe('Test suite 2', ({ test }) => {
 		test('Test', async () => {
 			await setTimeout(70);
