f9234c023c37d0a5cba615cf3081608a7462b599
Hiroki Osame <hiroki.osame@gmail.com>
2025-11-15 16:36:26 +0900

    test: migrate from tsd to expect-type

--- tests/index.test-d.ts
+++
@@ -1,5 +0,0 @@
-import { expectType } from 'tsd';
-import { testSuite } from '#manten';
-
-const importedTestSuite = testSuite((_context, _a: string, _b: number, _c: 'hello') => 1234);
-expectType<(_a: string, _b: number, _c: 'hello') => number>(importedTestSuite);
